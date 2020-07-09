2b1bad2f9e1914e4de350b51ab5cae04be29ba44
Ye Zhang <mr.yezhang@gmail.com>
2020-07-08 21:42:51 -0700

    Commend out BME chip check code, so the same library works with BMP as well.

--- bme280_i2c.py
+++ bme280_i2c.py
@@ -84,7 +84,7 @@ class BME280_I2C:
 
         # The cheaper BMP280 won't pass this function.
         # It only works with BME280.
-        self._read_chip_id()
+        # self._read_chip_id()
         self._soft_reset()
         self._load_calibration_data()
 
